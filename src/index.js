023f6c48455542109266448960c7c3ed00a764e2
loutiannan <loutiannan@wxyundian.com>
2020-10-20 14:08:00 +0800

    init route

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+// import App from './App';
+import AppRouter from './router/index'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AppRouter />
   </React.StrictMode>,
   document.getElementById('root')
 )
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Route, Router } from 'react-router-dom'
+import { Route,  BrowserRouter as Router } from 'react-router-dom'
 
 import Index from '../views/Index'
 
