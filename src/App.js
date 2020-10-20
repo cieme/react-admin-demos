05d399eb159fdfc79ddd979c0e422609a5cc1932
Cieme <aslslgood22@163.com>
2020-10-20 12:18:36 +0800

    :art: 改进结构

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import logo from './logo.svg';
-import './App.css';
+import "normalize-css/normalize.css"
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <Router>
+      <div>
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <nav >
+          <ul className="tabs">
+            <li className="list">
+              <Link to="/">Home</Link>
+            </li>
+            <li className="list">
+              <Link to="/about">About</Link>
+            </li>
+            <li className="list">
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+      </div>
+    </Router>
   );
 }
+function Home() {
+  return <h1 className="route">Home content</h1>;
+}
 
+function About() {
+  return <h2 className="route">About content</h2>;
+}
+
+function Users() {
+  return <h2 className="route">Users content</h2>;
+}
 export default App;
