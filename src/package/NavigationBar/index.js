1b02ce1e54bb2bc6137d94a7a7041fa6868f33f8
loutiannan <loutiannan@wxyundian.com>
2020-10-23 15:05:03 +0800

    feat

--- src/package/NavigationBar/index.js
+++ src/package/NavigationBar/index.js
@@ -4,7 +4,16 @@ import './index.scss'
 export default class NavigationBar extends Component {
   render() {
     return (
-      <div className="navigationbar">123</div>
+      <div className="navBar">
+        <div className="navBar-fixed">
+          <div className="icon"></div>
+          <div className="navBar-title">
+            <div className="prefix-icon"></div>
+            <div>标题</div>
+          </div>
+          <div className="icon"></div>
+        </div>
+      </div>
     )
   }
 }
